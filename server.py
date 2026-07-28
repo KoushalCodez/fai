5218a0e262ac33465073f410fda67def7399aa77
koushal <koushalhegde@gmail.com>
2026-07-28 20:29:27 +0530

    Stage 2: read endpoints with 404

--- server.py
+++ server.py
@@ -1,6 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 
 app = FastAPI()
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Complete FastAPI assignment",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Learn Pydantic models",
+        "done": True
+    },
+    {
+        "id": 3,
+        "title": "Build a Todo API",
+        "done": False
+    }
+]
+
+@app.get("/tasks")
+async def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+async def get_task(id: int):
+    try:
+        return tasks[id-1]
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+
 
 @app.get("/")
 async def root():
